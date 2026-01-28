29b7e1fddf46f09d0382af154b8057882d2765c5
Ahmed Barakat <ahmedbarakat1724@gmail.com>
2026-01-28 09:51:20 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./../node_modules/@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
 import './index.css'
-import "./app.css"
+import "./App.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
